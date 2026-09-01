e369c61658a520771d1b4641ae84f8a469727923
InfantryMovie <infantryman.damir@yandex.ru>
2026-09-01 14:40:16 +0300

    add Cat bean with prototype scope

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        System.out.println("Проверка равенства HelloWorld beans: " + (bean1 == bean2));
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getName());
+
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getName());
+
+        System.out.println("Проверка равенства Cat beans: " + (cat1 == cat2));
     }
 }
